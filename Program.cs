61c637f3336dd866b4092b34a4c855b00955fddf
Nadusha <nadusha230275@gmail.com>
2022-07-03 18:41:27 +0500

    работает

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿// показать последнию цифру трехзначного числа
 Console.WriteLine("введите трехзначное число число ");
 int a= int.Parse(Console.ReadLine());
+if (a>99 && a<1000)
+{
 Console.WriteLine("последняя цифра числа=");
 Console.Write(a % 10);
-
+}
+else 
+{
+    Console.WriteLine("Число не трехзначное");
+}
 
