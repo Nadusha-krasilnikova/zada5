bbf1628b10718ee84479a7c526fca5ad36c1c44d
Nadusha <nadusha230275@gmail.com>
2022-07-03 18:29:56 +0500

    вариант 1

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// показать последнию цифру трехзначного числа
+Console.WriteLine("введите трехзначное число число ");
+int a= int.Parse(Console.ReadLine());
+Console.WriteLine("последняя цифра числа=");
+Console.Write(a % 10);
+
+
